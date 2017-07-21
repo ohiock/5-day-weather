0f0854ff133b49a208cb49ae5c04e1d6ef7dfc20
ohiock <tbowman.oh@gmail.com>
2017-07-20 20:57:12 -0600

    Added css modules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -88,7 +88,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -118,7 +118,7 @@ module.exports = {
           {
             options: {
               formatter: eslintFormatter,
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -165,7 +165,7 @@ module.exports = {
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
         options: {
-          
+
           compact: true,
         },
       },
@@ -193,6 +193,7 @@ module.exports = {
                   options: {
                     importLoaders: 1,
                     minimize: true,
+                    modules: true,
                     sourceMap: true,
                   },
                 },
