7d73d2bb710b2f173719dc0b5343a689fe2675e3
Terrence Bowman <tbowman.oh@gmail.com>
2017-07-23 16:44:51 -0600

    Added react-css, react-bootstrap - may revisit implementation as this creates 2 css files, 2 requests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
 import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.app}>
+        <Grid>
+          <Row>
+            <Col md={2}>Hello</Col>
+            <Col md={10}>There</Col>
+          </Row>
+        </Grid>
         5 Day Weather
       </div>
     );
