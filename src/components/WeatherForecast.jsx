4aa5c0dc1b54d0199e2a20e29b6ba4f131800d95
Terrence Bowman <tbowman.oh@gmail.com>
2017-07-26 22:21:33 -0600

    Changed weather forecast component to be pure function

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
@@ -17,45 +17,41 @@ const propTypes = {
   goBackCallback: PropTypes.func.isRequired,
 };
 
-class WeatherForecast extends Component {
-  render() {
-    return (
-      <Grid fluid className={this.props.show ? sharedStyles.show : sharedStyles.hide}>
+const WeatherForecast = (props) => (
+  <Grid fluid className={props.show ? sharedStyles.show : sharedStyles.hide}>
+    <Row>
+      <Col lg={12}>
+        <div className={sharedStyles.textCenter}>
+          <h1 className={styles.forecastTitle}>5 Day Weather Forecast<br /> for {props.weatherForecast.city}</h1>
+        </div>
         <Row>
-          <Col lg={12}>
-            <div className={sharedStyles.textCenter}>
-              <h1 className={styles.forecastTitle}>5 Day Weather Forecast<br /> for {this.props.weatherForecast.city}</h1>
-            </div>
-            <Row>
-              {
-                this.props.weatherForecast.days
-                  .map((day, i) => (
-                    <Col xs={6} mdOffset={i === 0 ? 1 : 0} md={2} key={day.date} className={`${styles.forecastDay} ${styles[`background${i}`]}`}>
-                      <ForecastHeader
-                        weekDay={moment.unix(day.date).format('dddd')}
-                        today={moment().format('dddd')}
-                        tomorrow={moment().add(1, 'days').format('dddd')} />
-                      <div className={`${sharedStyles.textCenter} ${styles.icon}`}><ForecastIcon icon={day.icon} /></div>
-                      <div className={styles.temp}>{ `${day.temp}°` }</div>
-                      <div className={styles.condition}>{ day.condition }</div>
-                      <div>{ day.description }</div>
-                    </Col>
-                  ))
-              }
-            </Row>
-          </Col>
+          {
+            props.weatherForecast.days
+              .map((day, i) => (
+                <Col xs={6} mdOffset={i === 0 ? 1 : 0} md={2} key={day.date} className={`${styles.forecastDay} ${styles[`background${i}`]}`}>
+                  <ForecastHeader
+                    weekDay={moment.unix(day.date).format('dddd')}
+                    today={moment().format('dddd')}
+                    tomorrow={moment().add(1, 'days').format('dddd')} />
+                  <div className={`${sharedStyles.textCenter} ${styles.icon}`}><ForecastIcon icon={day.icon} /></div>
+                  <div className={styles.temp}>{ `${day.temp}°` }</div>
+                  <div className={styles.condition}>{ day.condition }</div>
+                  <div>{ day.description }</div>
+                </Col>
+              ))
+          }
         </Row>
-        <Row>
-          <Col>
-            <div className={`${styles.goBackContainer} ${sharedStyles.textCenter}`} onClick={this.props.goBackCallback}>
-              <span className={styles.goBackButton}>Search Again</span>
-            </div>
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
-}
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <div className={`${styles.goBackContainer} ${sharedStyles.textCenter}`} onClick={props.goBackCallback}>
+          <span className={styles.goBackButton}>Search Again</span>
+        </div>
+      </Col>
+    </Row>
+  </Grid>
+);
 
 WeatherForecast.propTypes = propTypes;
 
