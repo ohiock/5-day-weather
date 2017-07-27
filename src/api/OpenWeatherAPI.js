846514231031d5f0549ff6ed0b1ebb709d0a24ab
Terrence Bowman <tbowman.oh@gmail.com>
2017-07-27 01:27:53 -0600

    Added proxy, as site will be hosted from https and cannot hit http resource

--- src/api/OpenWeatherAPI.js
+++ src/api/OpenWeatherAPI.js
@@ -4,7 +4,7 @@ import Config from '../constants/Config';
 
 const OpenWeatherAPI = {
   getWeatherForecast(city) {
-    return axios.get('http://api.openweathermap.org/data/2.5/forecast/daily', {
+    return axios.get('https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast/daily', {
       params: {
         APPID: Config.APP_ID,
         cnt: Config.FORECAST_REQUEST_DAY_COUNT,
